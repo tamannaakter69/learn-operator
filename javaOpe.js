c18fabc9196daba539cc32f94b338eba56d5dbd6
tamannaakter69 <tamannaakter6998@gmail.com>
2024-05-27 13:57:42 +0600

    learn operator

---
+++ javaOpe.js
@@ -0,0 +1,57 @@
+// Operator:
+// 1. Arithmatic Operator
+// 2. Assainment Operator
+// 3. Comparision Operator
+// 4. Logical Operator
+
+
+// Arithmatic Operator
+//(+,-,*,/,**,%,++,--)
+// (++ atake increment bole, -- atake dicrement bole)
+
+
+// Assainment Operator
+//(=,+=,-=,*=,/=,++=,--=,%=)
+
+
+// Comparision Operator
+//(<,>,==,===,!=,>=,<=)
+
+
+//Logical Operator
+//(&&,||,!,&,|)
+
+
+// const num1 = 50;
+// const num2 = 10;
+// const sum = num1 + num2;
+// console.log(sum)
+
+
+//const num1 = 466793
+//const num2 = 789
+//const substract = num1 - num2
+//console.log(substract)
+
+
+
+//const num1 = 7536
+//const num2 = 2940
+//const multiply = num1 * num2
+//console.log(multiply)
+
+
+
+//const num1 = 23901739
+//const num2 = 4759
+//const divider = num1 / num2
+//console.log(divider)
+
+
+
+//const num1 = 25
+//const num2 = 10 
+//const modulas = num1 % num2
+//console.log(modulas)
+
+
