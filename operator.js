9a80828502dd4fc30e1410a43aaab9e087d08532
tamannaakter69 <tamannaakter6998@gmail.com>
2024-05-28 19:22:36 +0600

    let_ope

---
+++ operator.js
@@ -0,0 +1,21 @@
+//let num1 = 3;
+//let num2 = 8;
+//let sum = num1 + num2;
+//console.log(sum)
+
+//let num1 = 234890;
+//let num2 = 3737509;
+//let substract = num1 - num2;
+//console.log(substract)
+
+
+//let number1 = 23573893;
+//let number2 = 457947;
+//let multiply = number1 * number2;
+//console.log(multiply)
+
+
+let number1 = 23793;
+let number2 = 34;
+let divider = number1 / number2;
+console.log(divider)
