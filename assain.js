f764a03aab0cfda4c818a250f291d0241049a582
tamannaakter69 <tamannaakter6998@gmail.com>
2024-05-29 14:49:49 +0600

    Assainment Operator

--- assain.js
+++ assain.js
@@ -2,6 +2,44 @@
 //(=,+=,-=,++=,--=,%=,*=,/=,>>=,<<=)
 
 
-let number1 = 13;
-number1 += 10;
+//let number1 = 13;
+//number1 += 10;
+//console.log(number1)
+
+
+//let number = 45;
+//number += 2;
+//console.log(number)
+
+
+//let number = 56;
+//number += 4;
+//console.log(number)
+
+//let number = 34;
+//number = number++ +34;
+//console.log(number)
+
+
+//let number = 57;
+//number = ++number + 4;
+//console.log(number)
+
+
+//let number = 80;
+//number = --number + 2;
+//console.log(number)
+
+
+//let number = 78;
+//number -= 2;
+//console.log(number)
+
+//let number1 = 12;
+//let number2 = 5;
+//number = number1 - number2;
+//console.log(number)
+
+let number1 = 12;
+number1 -= 5;
 console.log(number1)
